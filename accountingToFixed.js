18dd2cb1c5b1628b4d5c4190a452d814c19145c2
tsbajwa <teranjit@gmail.com>
2017-04-29 14:13:42 +1000

    w

--- accountingToFixed.js
+++ accountingToFixed.js
@@ -4,23 +4,24 @@ function toFixedStringy(value, precision)  {
 
   //Find position of decimal point. 
   var decimalPosition = stringValue.indexOf(".");
-  // Remove decimal point from stringValue
-  var numberWithoutDecimal = stringValue.replace('.','');
+  
 
   // Find length of string (decimal removed)
+
   var stringLength = stringValue.length
   
   // If precision is longer then string, add '0' to end of string as many times as is needed
   if(stringLength - (decimalPosition + 1) < precision) {
     var zerosToAdd = Maths.abs(stringLength - (decimalPosition + 1) - precision);
     for (var i = 0; i < zerosToAdd; i++) {
-    numberWithoutDecimal + "0";
+    stringValue + "0";
     }
   } 
  
   // Splice string in order to add decimal in new spot i.e move decimal precision times over
   var posBeforeDecimal = precision + decimalPosition;
- 
+ // Remove decimal point from stringValue
+  var numberWithoutDecimal = stringValue.replace('.','');
   var stringBeforeDecimal = numberWithoutDecimal.slice(0, posBeforeDecimal);
   var stringAfterDecimal =  numberWithoutDecimal.slice(posBeforeDecimal)
   
